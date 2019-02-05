0e3c5a857d2b73a715619e6baa88b91b285a7b2e
GOKULNANTHAN <47317514+GOKULNANTHAN@users.noreply.github.com>
2019-02-05 11:39:48 +0530

    Create EVENODD.py

---
+++ EVENODD.py
@@ -0,0 +1,9 @@
+num =int(input("Enter any numbers:"))
+flag=num%2
+if flag==0:
+  print(num, "is an even number")
+elif flag == 1:
+  print(num, "is an odd number")
+else:
+    print("ERROR,INVALID OUPUT")
+  
